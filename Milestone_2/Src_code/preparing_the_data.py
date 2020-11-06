7d47357d0773e2b64765231e9351d0a68346c94a
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-06 20:57:03 +0100

    transforming code to functions

--- Milestone_2/Src_code/preparing_the_data.py
+++ Milestone_2/Src_code/preparing_the_data.py
@@ -1,31 +1,35 @@
 from loading_the_data import *
 
-batch_size = 128
 num_classes = 10
-epochs = 12
-
 
 img_rows, img_cols = 28, 28
 
+def format_data_x(train, test, img_rows, img_cols):
+    if K.image_data_format() == 'channels_first':
+        train = train.reshape(x_train.shape[0], 1, img_rows, img_cols)
+        test = test.reshape(x_test.shape[0], 1, img_rows, img_cols)
+        input_shape = (1, img_rows, img_cols)
+    else:
+        train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
+        test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
+        input_shape = (img_rows, img_cols, 1)
+        
+format_data_x(x_train, x_test, 28, 28)
 
+def normalize_x(train, test):
+    train = train.astype('float32')
+    test = test.astype('float32')
+    train /= 255
+    test /= 255
+    print('x_train shape:', train.shape)
+    print(train.shape[0], 'train samples')
+    print(test.shape[0], 'test samples')
 
-if K.image_data_format() == 'channels_first':
-    x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
-    x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
-    input_shape = (1, img_rows, img_cols)
-else:
-    x_train = x_train.reshape(x_train.shape[0], img_rows, img_cols, 1)
-    x_test = x_test.reshape(x_test.shape[0], img_rows, img_cols, 1)
-    input_shape = (img_rows, img_cols, 1)
+normalize_x(x_train, x_test)
 
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
-x_train /= 255
-x_test /= 255
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+def vectorize_y(train, test, num_classes):
+    train = keras.utils.to_categorical(train, num_classes)
+    test = keras.utils.to_categorical(test, num_classes)
+    
+vectorize_y(y_train, y_test, 10)
 
-
-y_train = keras.utils.to_categorical(y_train, num_classes)
-y_test = keras.utils.to_categorical(y_test, num_classes)
