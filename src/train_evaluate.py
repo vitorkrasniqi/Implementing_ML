64abbcf6d7b772ed7a28abd88c26cd675aec80ef
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-07 18:45:26 +0100

    adding functions to the code

--- src/train_evaluate.py
+++ src/train_evaluate.py
@@ -1,15 +1,16 @@
 from compile_save_model import *
-model = load_model('my_model.h5')
 
+model = load_model("my_model.h5")
 
-model.fit(x_train, y_train,
-          batch_size=batch_size,
-          epochs=epochs,
-          verbose=1,
-          validation_data=(x_test, y_test))
-		  
+def fit_model(batch_size, epochs):
+    model.fit(x_train, y_train,
+              batch_size=batch_size,
+              epochs=epochs,
+              verbose=1,
+              validation_data=(x_test, y_test))
+
+fit_model(128, 2)
 
 score = model.evaluate(x_test, y_test, verbose=0)
 print('Test loss:', score[0])
 print('Test accuracy:', score[1])
-
