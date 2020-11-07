3d9972f0a45f847a65f23b68afb20e5162e9338b
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-07 18:43:01 +0100

    adding functions to developing_the_model

--- src/developing_the_model.py
+++ src/developing_the_model.py
@@ -1,13 +1,17 @@
 from preparing_the_data import *
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                 activation='relu',
-                 input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+def develop_model(num_classes):
+    global model
+    model = Sequential()
+    model.add(Conv2D(32, kernel_size=(3, 3),
+                     activation='relu',
+                     input_shape=input_shape))
+    model.add(Conv2D(64, (3, 3), activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(num_classes, activation='softmax')) 
+
+develop_model(10)
