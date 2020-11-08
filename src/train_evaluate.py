f118f6b9ceaac8742b2a0920d025760f1aaeae01
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-08 18:47:28 +0100

    updating with 2 functions

--- src/train_evaluate.py
+++ src/train_evaluate.py
@@ -1,6 +1,8 @@
 from compile_save_model import *
 
-model = load_model("my_model.h5")
+def model_load():
+    global model
+    model = load_model("my_model.h5")
 
 def fit_model(batch_size, epochs):
     model.fit(x_train, y_train,
@@ -9,8 +11,8 @@ def fit_model(batch_size, epochs):
               verbose=1,
               validation_data=(x_test, y_test))
 
-fit_model(128, 12)
-
-score = model.evaluate(x_test, y_test, verbose=0)
-print('Test loss:', score[0])
-print('Test accuracy:', score[1])
+def scoring():
+    global score
+    score = model.evaluate(x_test, y_test, verbose=0)
+    print('Test loss:', score[0])
+    print('Test accuracy:', score[1])
