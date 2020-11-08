aa025d5fa5edb5925cbcd7f44cb51115d04e7464
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-08 18:42:07 +0100

    adding function  data_load to the code

--- src/loading_the_data.py
+++ src/loading_the_data.py
@@ -7,4 +7,9 @@ from keras.layers import Conv2D, MaxPooling2D
 from keras import backend as K
 from keras.models import load_model
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
+def data_load():
+    global x_train
+    global y_train
+    global x_test
+    global y_test
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
