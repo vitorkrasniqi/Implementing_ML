c2ebe102a5a839218e06eae651476889f4e598b6
Michaela Havlíčková <72206626+michaelahavl@users.noreply.github.com>
2020-11-10 18:14:47 +0100

    adding predict function

--- src/load_model_predict.py
+++ src/load_model_predict.py
@@ -4,5 +4,7 @@ def model_load():
     global model
     model = load_model("my_fitted_model.h5")
 
-
-    
+def predict(x):
+    global pred
+    pred = model.predict_classes(x, verbose = 1)
+    return pred
